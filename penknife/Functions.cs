78f9b062b8395beea8f0b05b2cd7765526113f88
jh2015 <helblingjoel@gmail.com>
2020-03-08 20:50:07 +0000

    Implemented Length and Reverse methods

---
+++ penknife/Functions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace penknife
+{
+    public class Functions
+    {
+        public Functions()
+        {
+            
+        }
+
+        public static int Length(String word)
+        {
+            return word.Length;
+        }
+
+        public static string Reverse(String word)
+        {
+            char[] CharArray = word.ToCharArray();
+            Array.Reverse(CharArray);
+            return CharArray.ToString();
+        }
+    }
+}
--- penknife/Program.cs
+++ penknife/Program.cs
@@ -6,7 +6,23 @@ namespace penknife
     {
         static void Main(string[] args)
         {
-            
+            if (args.Length == 0)
+            {
+                // Display the menu with different options and stuff
+            }
+
+            if ((args[0].Equals("length") || args[0].Equals("size")) && args.Length == 2)
+                Console.Write("\n> " + Functions.Length(args[1]));
+
+            else if (args[0].Equals("reverse"))
+            {
+                if (args[1].Equals(null))
+                    Console.WriteLine("Reverse requires a string, use. use penknife -reverse string");
+                else
+                    Console.Write("\n> " + Functions.Reverse(args[1]));
+            }
+                
+
         }
     }
 }
