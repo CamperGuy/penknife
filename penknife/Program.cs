ccf39d60fc78fc94a05d322934a7c7f36e539446
Joel Helbling <helblingjoel@gmail.com>
2020-03-08 20:33:33 +0000

    Initial Commit

---
+++ penknife/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace penknife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
